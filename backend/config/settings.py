cbe33d10d7008c0a043faf51d712b1170d8f5f5e
Sanket Prajapati <prajapatisanketssp321321@gmail.com>
2026-05-16 15:02:01 +0530

    Final Fix: Verified absolute cloud urls across ther entire

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -176,6 +176,7 @@ DEFAULT_FROM_EMAIL = os.environ.get(
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
